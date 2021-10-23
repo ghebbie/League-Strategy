86a4170787de82b7f70021f15258d4ce9f31f4b7
DaveDuck <dev.programsofdaveduck@gmail.com>
2021-10-23 15:27:15 +0200

    Added functionality for creating maps for ChampClass

--- ChampClass.java
+++ ChampClass.java
@@ -1,10 +1,24 @@
 // This is the class that keeps track of a champions class e.g. vanguard, marksman, etc
+import java.util.*;
+
 
 public class ChampClass {
 	// Somewhere else we will have a charToInt() function that turns the grade into a number
-	private char attack;
-	private char catching; // catch is a reserved word appararently
-	private char protect;
-	private char siege;
-	private char split;
+	private String name;
+	private HashMap<String, Integer> comps;
+
+	public ChampClass(String name, 
+					  int attack, 
+					  int catching, 
+					  int protect,
+					  int siege,
+					  int split){
+		this.name = name;
+		comps = new HashMap<>();
+		comps.put("attack", attack);
+	}
+
+	public String getName(){
+		return name;
+	}
 }
